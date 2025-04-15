e0b210d701cc8bb492373b733b5e74c00ac760d8
Silvehr <142506674+Silvehr@users.noreply.github.com>
2025-04-15 19:31:35 +0200

    Create bisekcja_calki.cpp

---
+++ bisekcja_calki.cpp
@@ -0,0 +1,56 @@
+double find0(double a, double b, double delta, double epsilon)
+{
+    double rangeLen;
+    double x0;
+
+    while(true)
+    {
+        rangeLen = rangeLen / 2;
+        x0 = a + rangeLen;
+
+        // w *function* wstaw swoją funkcje
+        if(abs(rangeLen) < delta || abs(function(x0)) < epsilon)
+            return x0;
+        
+        if(function(x0) * function(a) < 0)
+            b = x0;
+        else
+            a = x0;
+    }
+}  
+
+double CalkaProstokat(double a, double b, int p)
+{
+    if(a == b)
+        return 0;
+
+    double h = (b - a) / precision;
+    double pole = 0;
+
+    for(double current = a + step; current <= b; current+=h)
+    {
+        // zamiast *func* wstaw nazwę funkcji
+        pole += abs(func(current)) * h;
+    }
+
+    return pole;
+} 
+
+double CalkaTrapez(double a, double b, int p)
+{
+    if(a == b)
+        return 0;
+
+    double h = (a - b) / p;
+    double prev = a;
+    double pole = 0;
+
+    for(double current =a + h; current <= b; current+=h)
+    {
+        // func => twoja funkcja
+        pole += (abs(func(current) + func(prev))) * h / 2;
+        prev = current;
+    }
+
+    return pole;
+}
